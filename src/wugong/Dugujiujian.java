5008ca34e1c2f29070c7f98d5b32f19b3b023a5d
zhoujianyu <zjy_zhoujianyu@yeah.net>
2020-10-26 19:48:06 +0800

    3333

--- src/wugong/Dugujiujian.java
+++ src/wugong/Dugujiujian.java
@@ -3,5 +3,7 @@ package wugong;
 public class Dugujiujian {
 	public static void main(String[] args) {
 		System.out.println("111111");
+		System.out.println("222222");
+		System.out.println("333333");
 	}
 }
